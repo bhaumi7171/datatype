52e74a5de8b3e5f59e4377c0d73c7fc2670a83e4
binoo <binoo@BHAUMI>
2024-10-10 12:56:56 +0530

    datatype project updated

---
+++ JavaSample/src/main/java/datatype/First_Datatype.java
@@ -0,0 +1,23 @@
+package datatype;
+
+public class First_Datatype {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		System.out.println("Integer Number "+a);
+		char d='B';
+		System.out.println("Character Is "+d);
+		short b=123;
+		System.out.println("Short Is "+b);
+		long c=124563;
+		System.out.println("Long Is "+c);
+		float e=23.4f;
+		System.out.println("Float Is "+e);
+		double f=45.45625d;
+		System.out.println("Double Is "+f);
+		String g="qwerty";
+		System.out.println("String Is "+g);
+	}
+
+}
